8b99ebaec9ae7c5610e622944e5ce9c47925df6e
CHIRANTH RAJU C <122692170+CHIRANTH-24@users.noreply.github.com>
2024-08-10 20:36:55 +0530

    feat: addSlot page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,14 @@ import Home from "./pages/home";
 import Login from "./pages/Login";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./routes/ProtectedRoute";
-
+import AddSlot from "./pages/addSlot";
 const App = () => {
   return (
     <BrowserRouter>
     <AuthProvider>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/addSlot" element={<AddSlot /> } />
           <Route element={<ProtectedRoute />}>
             <Route path="/" element={<Home />} />
           </Route>
